6290cddd8bb6cfc0b82b2a9d2f811569b413bbda
alcl2000 <amy.555.lewis@gmail.com>
2023-06-12 13:57:00 +0000

    add: navbar element

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav, Container} from 'react-bootstrap';
+import logo from '../assets/logo.png'
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href='#home'>
+                    <img src={logo} alt="" height={'45px'}/>
+                </Navbar.Brand>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#home">Register</Nav.Link>
+                                <Nav.Link href="#link">Login</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </div>
   )
 }
 
