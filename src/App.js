3850594dad115b3e5523f98fd68ae5e420975f29
alcl2000 <amy.555.lewis@gmail.com>
2023-06-12 16:30:55 +0000

    add: signin route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Switch, Route} from "react-router-dom";
+import  {Container} from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Container>
+        <Route exact path='/signin' render={() => <h1>Sign In page</h1>}/>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 const NavBar = () => {
   return (
     <div>
         <Navbar className={styles.NavBar} expand="lg">
             <Container>
-                <Navbar.Brand href='#home'>
-                    <img src={logo} alt="" height={'45px'}/>
-                </Navbar.Brand>
-                <Navbar.Brand className={styles.Title} href="#home">Crack-It</Navbar.Brand>
+                <NavLink to='/'>
+                    <Navbar.Brand>
+                        <img src={logo} alt="" height={'45px'}/>
+                    </Navbar.Brand>
+                    <Navbar.Brand className={styles.Title}>Crack-It</Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse className={styles.collapse} id="basic-navbar-nav">
                         <Nav className="ml-auto text-left">
-                            <Nav.Link href="#login"><i class="fa-solid fa-right-to-bracket"></i></Nav.Link>
-                            <Nav.Link href="#register"><i class="fa-solid fa-user-plus"></i></Nav.Link>
+                            <NavLink to="/signin"><i class="fa-solid fa-right-to-bracket"></i></NavLink>
+                            <NavLink to="/register"><i class="fa-solid fa-user-plus"></i></NavLink>
                         </Nav>
                     </Navbar.Collapse>
             </Container>
