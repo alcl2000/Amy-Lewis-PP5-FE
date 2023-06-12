d9b32c0f492c4ea665cb51e08eea5ab0c39d40f9
alcl2000 <amy.555.lewis@gmail.com>
2023-06-12 11:45:29 +0000

    add: navbar comp created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <NavBar />
     </div>
   );
 }
