a35ef77cc6baae86b954ac3c2babe1a93d2c8a10
alcl2000 <amy.555.lewis@gmail.com>
2023-06-12 15:33:23 +0000

    add: react router installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Switch, Route} from "react-router-dom";
 
 function App() {
   return (
