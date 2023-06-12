09ca94fab35030fd508b76e7ac8b62403be45bd2
alcl2000 <amy.555.lewis@gmail.com>
2023-06-12 17:59:11 +0000

    add: signup form route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Switch, Route} from "react-router-dom";
 import  {Container} from "react-bootstrap"
+import './api/axiosDefaults'
+import SignUpForm from './pages/auth/SignUpForm'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Container>
-        <Route exact path='/signin' render={() => <h1>Sign In page</h1>}/>
+        <Route exact path='/signin' render={() => <SignUpForm />}/>
       </Container>
     </div>
   );
