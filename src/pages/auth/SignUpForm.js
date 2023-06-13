30e89467de78313cb9339ff088109f19b51f49a1
alcl2000 <amy.555.lewis@gmail.com>
2023-06-13 11:02:43 +0000

    add: form styling and link

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import {Form, Container, Button } from 'react-bootstrap'
+import styles from '../../styles/SignUpForm.module.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const SignUpForm = () => {
     return (
         <div>
-            <h1>Create an account</h1>
-            <Container>
+            <h1 className={styles.Title}>Create an account</h1>
+            <Container className={styles.Form}>
                 <Form>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label srOnly>Username</Form.Label>
+                        <Form.Label >Username</Form.Label>
                         <Form.Control type="email" placeholder="Username" />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label srOnly>Password</Form.Label>
+                        <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label srOnly>Confirm Password</Form.Label>
                         <Form.Control type="password" placeholder="Confirm Password" />
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="info" type="submit" block>
                         Create Account
                     </Button>
                 </Form>
+            </Container>
+            <Container className={styles.Subtext}>
+                    <p className='muted' pt='5px'>Already have an account?</p>
+                    <Link >Log In</Link>
             </Container>
         </div>
     )
