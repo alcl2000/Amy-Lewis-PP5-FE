575666709b097ea40fbb444259742ffddfa6a861
alcl2000 <amy.555.lewis@gmail.com>
2023-06-13 11:25:27 +0000

    add: handle change sign up data

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
 import {Form, Container, Button } from 'react-bootstrap'
 import styles from '../../styles/SignUpForm.module.css'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const SignUpForm = () => {
+    /* create account data */
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password1: '',
+        password2: ''
+    });
+    const {username, password1, password2} = signUpData;
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value, 
+        });
+    };
+    /* JSX return */
     return (
         <div>
-            <h1 className={styles.Title}>Create an account</h1>
             <Container className={styles.Form}>
+                <h1 className={styles.Title}>Create an account</h1>
                 <Form>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label >Username</Form.Label>
-                        <Form.Control type="email" placeholder="Username" />
+                        <Form.Control 
+                            type="text" 
+                            placeholder="Username"
+                            value={username}
+                            onChange={handleChange}
+                        />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control 
+                            type="password" 
+                            placeholder="Password"
+                            value={password1}
+                            onChange={handleChange}
+                        />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label srOnly>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm Password" />
+                        <Form.Control 
+                            type="password" 
+                            placeholder="Confirm Password"
+                            value={password2}
+                            onChange={handleChange}
+                        />
                     </Form.Group>
                     <Button variant="info" type="submit" block>
                         Create Account
