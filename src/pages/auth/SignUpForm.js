295ab0a2923893bb4f8c0bd354de2d6a859f7222
alcl2000 <amy.555.lewis@gmail.com>
2023-06-13 14:06:22 +0000

    add: handlesubmit

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import {Form, Container, Button } from 'react-bootstrap'
 import styles from '../../styles/SignUpForm.module.css'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import axios from "axios";
 
 const SignUpForm = () => {
     /* create account data */
@@ -11,18 +12,33 @@ const SignUpForm = () => {
         password2: ''
     });
     const {username, password1, password2} = signUpData;
+    /*event handling */
+    const history = useHistory()   
     const handleChange = (event) => {
         setSignUpData({
             ...signUpData,
             [event.target.name]: event.target.value, 
         });
     };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await axios.post('dj-rest-auth/registration/', signUpData);
+            history.push('/signin')
+        } catch (err) {
+            setErrors = err.response?.data
+        }
+    };
+    /*error handling */
+    const [errors, setErrors] = useState({
+
+    })
     /* JSX return */
     return (
         <div>
             <Container className={styles.Form}>
-                <h1 className={styles.Title}>Create an account</h1>
-                <Form>
+                <h3 className={styles.Title}>Create an account</h3>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label >Username</Form.Label>
                         <Form.Control 
