8313c58cd41ea1b8fe6c2ff85898e234d42163ca
alcl2000 <amy.555.lewis@gmail.com>
2023-06-14 10:52:05 +0000

    fix: content type error fixed

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://crack-it-api.herokuapp.com/'
-axios.defaults.headers.post['Content type'] = 'multipart/form-data'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
