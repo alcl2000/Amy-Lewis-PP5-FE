0213c3717cc8cb1b43ee014edea1555faa377cda
alcl2000 <amy.555.lewis@gmail.com>
2023-06-14 11:03:55 +0000

    add: signin form component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { Switch, Route} from "react-router-dom";
 import  {Container} from "react-bootstrap"
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm'
+import SignInForm from './pages/auth/SignInForm'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Container>
-        <Route exact path='/signin' render={() => <SignUpForm />}/>
+        <Route exact path='/register' render={() => <SignUpForm />}/>
+        <Route exact path='/signin' render={() => <SignInForm />}/>
       </Container>
     </div>
   );
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const SignInForm = () =>{
+
+    return(
+        <div>
+            <h1>HELLO</h1>
+        </div>
+    );
+
+};
+
+export default SignInForm;
