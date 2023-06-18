37b34501e970b28462f72b7540ef4e30bce1f681
alcl2000 <amy.555.lewis@gmail.com>
2023-06-18 15:16:02 +0000

    fix: current user context refactor

--- src/contexts/CurrentUserContexts.js
+++ src/contexts/CurrentUserContexts.js
@@ -1,34 +1,30 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import axios from 'axios';
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosRes } from "../api/axiosDefaults";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
 
-export const useCurrentUser = () => useContext(CurrentUserContext)
-export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
-export const CurrentUserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null)
-
-    const handleMount = async () => {
-        try {
-            const {data} = await axios.get('dj-rest-auth/user/')
-            setCurrentUser(data)
-        } catch (err){
-            console.log(err)
-        }
-    };
-
-    useEffect(() => {
-        handleMount()
-    }, []);
-    /*JSX return */
-    return(
-        <CurrentUserContext.Provider value={currentUser}>
-            <SetCurrentUserContext.Provider value={currentUser}>
-                {children}
-            </SetCurrentUserContext.Provider>
-        </CurrentUserContext.Provider>
-    );
-
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    handleMount();
+  }, []);
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
 };
