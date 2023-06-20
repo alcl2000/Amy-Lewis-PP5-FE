fa2f61ebae11f68c59fa0dd6a8972156e216a61e
alcl2000 <amy.555.lewis@gmail.com>
2023-06-20 15:35:06 +0000

    add: non-logged in user page view

--- src/pages/projects/ProjectCreateForm.js
+++ src/pages/projects/ProjectCreateForm.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { useCurrentUser } from '../../contexts/CurrentUserContexts';
+import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ProjectCreateForm = () => {
-  return (
-    <h1>Pwoject form uwu</h1>
-  )
+    //User logic
+    const currentUser = useCurrentUser()
+    const loggedOutUserPage = (
+        <>
+            <div>
+                <p>Sorry! Only logged in users can create projects!</p>
+                <p>You can either:</p>
+                <Row>
+                    <Col>
+                        <Link className='btn btn-large btn-info' exact to='/signin'>Log In</Link>
+                    </Col>
+                    - or -
+                    <Col>
+                        <Link className='btn btn-large btn-info' exact to='/register'>Create an account</Link>
+                    </Col>
+                </Row>
+            </div>
+        </>
+    );
+    const loggedInUserPage = (
+        <>
+            <p>
+                form form formy form
+            </p>
+        </>
+    )
+    //JSX return statement
+    return (
+        <div>
+            <h1>Create a New project</h1>
+            {currentUser ? loggedInUserPage : loggedOutUserPage}
+        </div>
+    );
 }
 
 export default ProjectCreateForm
