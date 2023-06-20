ec2d6022b1492375fde662ccc89953202d5f290d
alcl2000 <amy.555.lewis@gmail.com>
2023-06-20 16:45:45 +0000

    add: form controls for main fields

--- src/pages/projects/ProjectCreateForm.js
+++ src/pages/projects/ProjectCreateForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useCurrentUser } from '../../contexts/CurrentUserContexts';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ProjectCreateForm = () => {
@@ -25,9 +25,33 @@ const ProjectCreateForm = () => {
     );
     const loggedInUserPage = (
         <>
-            <p>
-                form form formy form
-            </p>
+            <Container>
+                <Form>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Title" />
+                        <Form.Text>Give your project a unique, memorable name!</Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Goals</Form.Label>
+                        <Form.Text>You can set up to three goals for your project!</Form.Text>
+                        <Form.Control type="text" placeholder="Goal 1" />
+                        <Form.Control type="text" placeholder="Goal 2" />
+                        <Form.Control type="text" placeholder="Goal 3" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Pick a colour</Form.Label>
+                        <Form.Text>Make your project stand out and make it easier to identify!</Form.Text>
+                        <Form.Control as="select">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
+            </Container>
         </>
     )
     //JSX return statement
