994e914ac271ce43064070535693e8c88e25960d
alcl2000 <amy.555.lewis@gmail.com>
2023-06-21 17:55:47 +0000

    add: axios get request

--- src/pages/profiles/ProfileDetail.js
+++ src/pages/profiles/ProfileDetail.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { useCurrentUser } from '../../contexts/CurrentUserContexts';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../api/axiosDefaults';
 
-const ProfileDetail = () => {
+
+const ProfileDetail = (props) => {
+    const {id} = useParams();
+    const [profileData, setProfileData] = useState({
+        username: "",
+        id: id
+    });
+    
+    // User context + owner context
+    const currentUser = useCurrentUser();
+    const isOwner = currentUser?.id === profileData?.id;
+    //request
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const {data} =  await axiosReq.get(`/profiles/${id}`)
+                setProfileData(data)
+            } catch (err){
+                console.log(err, 'profile')
+            }
+        }
+    })
   return (
-    <div>ProfileDetail</div>
+    <Container>
+        <h2>{profileData?.username}</h2>
+        <h3>{profileData?.id}</h3>
+    </Container>
   )
 }
 
