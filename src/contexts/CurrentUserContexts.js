e51edc473cf4622f25f68cf2dc9a29adc6d7a764
alcl2000 <amy.555.lewis@gmail.com>
2023-06-21 14:09:20 +0000

    fix: currentusercontext updated

--- src/contexts/CurrentUserContexts.js
+++ src/contexts/CurrentUserContexts.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
-import { axiosReq, axiosRes } from "../api/axiosDefaults";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import axios from "axios";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { useHistory } from "react-router";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -11,7 +11,7 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-  const history = useHistory;
+  const history = useHistory();
 
   const handleMount = async () => {
     try {
@@ -21,33 +21,34 @@ export const CurrentUserProvider = ({ children }) => {
       console.log(err);
     }
   };
+
   useEffect(() => {
     handleMount();
   }, []);
 
   useMemo(() => {
     axiosReq.interceptors.request.use(
-        async (config) => {
-          try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-          } catch (err) {
-            setCurrentUser((prevCurrentUser) => {
-              if (prevCurrentUser) {
-                history.push("/signin");
-              }
-              return null;
-            });
-            return config;
-          }
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
           return config;
-        },
-        (err) => {
-          return Promise.reject(err);
         }
-      );
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
 
     axiosRes.interceptors.response.use(
-        (response) => response,
+      (response) => response,
       async (err) => {
         if (err.response?.status === 401) {
           try {
