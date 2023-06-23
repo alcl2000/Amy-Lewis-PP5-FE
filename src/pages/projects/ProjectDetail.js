c67212cf66bfc55b1e18d9a72984e074f69e5943
alcl2000 <amy.555.lewis@gmail.com>
2023-06-23 14:16:23 +0000

    add: details

--- src/pages/projects/ProjectDetail.js
+++ src/pages/projects/ProjectDetail.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Container, Col, Row} from 'react-bootstrap'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 const ProjectDetail = () => {
   return (
-    <div>ProjectDetail</div>
+    <section>
+        <Container>
+            <Row>
+                <Col sm={3}>
+                    <h3>Project:</h3>
+                </Col>
+                <Col sm={6}></Col>
+                <Col sm={3}>
+                    <h3>Days remaining:</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={4}>Goal 1:</Col>
+                <Col sm={4}>Goal 2:</Col>
+                <Col sm={4}>Goal 3:</Col>
+            </Row>
+            <Link to='/tasks/create' className='btn btn-large pill btn-info rounded-pill'>+ Add a new Task</Link>
+        </Container>
+        <Container>
+            <Row>
+                <Col sm={3}>
+                    Tasks:
+                </Col>
+                <Col sm={6}></Col>
+                <Col sm={3}>
+                    Sort by:
+                </Col>
+            </Row>
+        </Container>
+    </section>
   )
 }
 
