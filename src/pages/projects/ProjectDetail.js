2974091050e27dbbd8777cb743b79dab9370a1f6
alcl2000 <amy.555.lewis@gmail.com>
2023-06-24 19:37:14 +0000

    add: colour state

--- src/pages/projects/ProjectDetail.js
+++ src/pages/projects/ProjectDetail.js
@@ -10,12 +10,14 @@ const ProjectDetail = () => {
     const {id} = useParams();
     const [hasLoaded, setHasLoaded] = useState(false);
     const [projectData, setProjectData] = useState({})
+    const [colorDetail, setColorDetail] = useState();
     //GET request
     useEffect( () => {
         const fetchData = async () => {
             try{
                 const data = await axiosReq.get(`/projects/${id}`);
                 setProjectData(data.data);
+                setColorDetail(data.data.color)
                 setHasLoaded(true)
             } catch (err){
                 console.log(err)
@@ -23,6 +25,10 @@ const ProjectDetail = () => {
         };
         fetchData();
     }, [setProjectData, id])
+    //Goals
+    
+
+    
     // has loaded states
     const loadedFalse = <>
                         <Avatar src={loading} height={200} />
@@ -36,15 +42,18 @@ const ProjectDetail = () => {
                 </Col>
                 <Col sm={6}></Col>
                 <Col sm={3}>
-                    <h3>Days remaining:</h3>
+                    <h3>Deadline: {projectData.deadline}</h3>
                 </Col>
             </Row>
             <Row>
-                <Col sm={4}>Goal 1:</Col>
-                <Col sm={4}>Goal 2:</Col>
-                <Col sm={4}>Goal 3:</Col>
+                <Col sm={4}>Goal 1: {projectData.goal_1}</Col>
+                <Col sm={4}>Goal 2: {projectData.goal_2}</Col>
+                <Col sm={4}>Goal 3: {projectData.goal_3}</Col>
             </Row>
-            <Link to='/tasks/create' className='btn btn-large pill btn-info rounded-pill'>+ Add a new Task</Link>
+            <Row>
+                <Col sm={6}><Link to='/tasks/create' className='btn btn-large pill btn-info rounded-pill'>+ Add a new Task</Link></Col>
+                <Col sm={6}>Add new members</Col>
+            </Row>
         </Container>
         <Container>
             <Row>
