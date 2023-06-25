2b622368a6a2c7582c96cc93af5c7d0fb567e0f6
alcl2000 <amy.555.lewis@gmail.com>
2023-06-25 16:41:16 +0000

    add: submit logic

--- src/pages/tasks/TaskCreateForm.js
+++ src/pages/tasks/TaskCreateForm.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Form, Row, Col, Button} from 'react-bootstrap'
 import styles from '../../styles/TaskCreateForm.module.css'
+import { useCurrentUser } from '../../contexts/CurrentUserContexts'
+import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { axiosReq } from '../../api/axiosDefaults'
 
 const TaskCreateForm = () => {
+    //Form logic
+    const currentUser = useCurrentUser();
+    const projectId = useParams();
+    const [taskData, setTaskData] =  useState({
+        project: projectId,
+        title: "", 
+        important: false,
+        progress: "not_started",
+        due_date: "",
+    });
+    const {project, title, important, progress, due_date} = taskData;
+    const [errors, setErrors] = useState({});
+    //Subumit logic
+    const history = useHistory();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            const {data} = await axiosReq.post(`/tasks/`, taskData);
+            history.push(`tasks/${data.id}`)
+        } catch(err){
+            setErrors(err.response?.data);
+        }
+    }
+    //Change logic
+    const handleChange = () => {
+        console.log(project)
+    }
     //Return statement 
     return (
         <div>
@@ -14,13 +44,20 @@ const TaskCreateForm = () => {
                     <Form.Group as={Row}>
                         <Form.Label column sm={6}>Creating new task in project:</Form.Label>
                         <Col sm={6}>
-                            <Form.Control type="text" placeholder="Project name here" readOnly plaintext/>
+                            <Form.Control type="text" 
+                                placeholder={project}
+                                readOnly 
+                                plaintext
+                                name='project'
+                                value={project}
+                                />
                         </Col>
                     </Form.Group>
                     <Form.Group controlId="formGroupEmail" as={Row}>
                         <Form.Label column sm={6}>Description</Form.Label>
                         <Col sm={6}>
-                            <Form.Control type="text" placeholder="Describe task" />
+                            <Form.Control type="text" placeholder="Describe task" 
+                                onChange={handleChange}/>
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row}>
@@ -33,6 +70,13 @@ const TaskCreateForm = () => {
                                 label="Important task"
                             />
                         </Col>
+                    </Form.Group>
+                    <Form.Group controlId="deadline">
+                        <Form.Label>Set a deadline</Form.Label>
+                        <Form.Text>Picking a deadline can help you stay on track to achieve your goals!</Form.Text>
+                        <input type='datetime-local'
+                            name='deadline' 
+                        />
                     </Form.Group>
                     <Button type='submit' variant='info'>Create Task</Button>
                 </Form>
