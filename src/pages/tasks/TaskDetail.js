dceb6a20ee9e3763320f668bdade7db3578712e1
alcl2000 <amy.555.lewis@gmail.com>
2023-06-26 09:59:54 +0000

    add: Get request

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { axiosReq } from '../../api/axiosDefaults';
 
 const TaskDetail = () => {
-  return (
-    <div>TaskDetail</div>
-  )
+    const {id} = useParams();
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const [taskData, setTaskData] = useState({
+        title: "",
+    });
+    //Get statements
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const data = await axiosReq.get(`/tasks/${id}`);
+                console.log(data)
+                setTaskData(data.data)
+                setHasLoaded(true)
+            } catch(err){
+                console.log(err)
+            }
+        };
+        fetchData();
+    }, [setTaskData, id])
+
+    // JSX return statement
+    return (
+    <div>
+        <h1>{taskData.title}</h1>
+    </div>
+    )
 }
 
 export default TaskDetail
