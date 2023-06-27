5df347aeb7ec2eab8e091cf263bb218e5874d414
alcl2000 <amy.555.lewis@gmail.com>
2023-06-27 21:41:03 +0000

    fix: status message plaintext

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -7,6 +7,7 @@ import Avatar from '../../components/Avatar'
 import styles from '../../styles/TaskDetail.module.css'
 import axios from 'axios'
 
+
 const TaskDetail = () => {
     const {id} = useParams();
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -22,15 +23,28 @@ const TaskDetail = () => {
         const fetchData = async () => {
             try{
                 const data = await axiosReq.get(`/tasks/${id}`);
-                console.log(data)
-                setTaskData(data.data)
-                setHasLoaded(true)
+                setTaskData(data.data);
+                setHasLoaded(true);
             } catch(err){
                 console.log(err)
             }
         };
         fetchData();
     }, [setTaskData, id])
+    // Status display 
+    const statusChoices = (taskData) => {
+        let statusMessage
+        if (taskData.progress === 'not_started'){
+            statusMessage = "Not started";
+        } 
+        else if ( taskData.progress === 'in_progress'){
+            statusMessage = 'In progress';
+        } 
+        else{
+            statusMessage = 'Completed!';
+        }
+        return statusMessage
+    }
     //Delete handler
     const handleDelete = () => {
         setShowPopUp({
@@ -65,7 +79,8 @@ const TaskDetail = () => {
                     {taskData.important ? importantIcon : notImportantIcon }
                     <h3>Due: {taskData.due_date}</h3>
                     <h4>{taskData.title}</h4>
-                    <h4>{taskData.progress}</h4>
+                    <h4>{statusChoices(taskData)}</h4>
+                    {taskData.is_owner ? <p>[Edit task?]</p> : <></>}
                 </Container>
                 <h4 onClick={handleDelete}>[Delete Task?]</h4>
                 {/* Popup modal */}
