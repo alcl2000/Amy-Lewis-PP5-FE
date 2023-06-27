14769b5e0787e0af3657f14db51e69a64d4c1eaa
alcl2000 <amy.555.lewis@gmail.com>
2023-06-27 10:16:54 +0000

    fix: sidebar styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
-import styles from '../styles/SideBar.css'
-import TaskDisplay from './TaskDisplay'
+import styles from '../styles/SideBar.module.css'
+import TaskCard from './TaskCard'
 
 export const SideBar = () => {
   return (
     <div className={styles.Container}>
-        <TaskDisplay />
+        <TaskCard />
         <Link className='btn btn-large btn-info' to='/'>Add some here!</Link>
     </div>
   )
