809166e6e4ed964013ef2568796bd1d7726be549
alcl2000 <amy.555.lewis@gmail.com>
2023-06-27 17:40:19 +0000

    add: task card mounting

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import styles from '../styles/SideBar.module.css'
 import TaskCard from './TaskCard'
+import {useCurrentUser } from '../contexts/CurrentUserContexts'
+import {axiosReq} from '../api/axiosDefaults'
 
 export const SideBar = () => {
+    const [taskData, setTaskData] = useState([]);
+    const currentUser = useCurrentUser()
+    const user = currentUser.username;
+    console.log(user)
+    useEffect( () => {
+        const fetchData = async () => {
+            try{
+                const data = await axiosReq.get(`/tasks/?filter/user=${user}`)
+                console.log(currentUser)
+                setTaskData(data.data.results)                             
+            } catch (err){
+                console.log(err)
+            };
+        };
+        fetchData();
+        console.log(taskData)
+    },[setTaskData, currentUser])
   return (
     <div className={styles.Container}>
-        <TaskCard />
+        <TaskCard data={taskData}/>
         <Link className='btn btn-large btn-info' to='/'>Add some here!</Link>
     </div>
   )
