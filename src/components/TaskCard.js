7ed793608c6bec717e2c8a2f8393c2eac14e98d2
alcl2000 <amy.555.lewis@gmail.com>
2023-08-04 15:12:09 +0000

    add: task card content

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,56 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import {Card, Col, Row} from 'react-bootstrap'
-import Avatar from './Avatar';
-import loader from '../assets/loading.gif'
-import {axiosReq} from '../api/axiosDefaults'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import React from 'react'
+import {Card} from 'react-bootstrap'
 
-const TaskCard = () => {
-    //data mapping
-    const [taskData, setTaskData] = useState([]);
-    const [hasLoaded, setHasLoaded] = useState(true);
-    const {id} = useParams
-
-    useEffect(() => { 
-        console.log('mounted')
-        const fetchData = async () => {
-            try{   
-                const {data} = await axiosReq.get(`/tasks/`);
-                    setTaskData(data.results)
-                    setHasLoaded(true)
-                    console.log(taskData)
-            } catch(err){
-                console.log(err)
-        };
-        fetchData()
-        }},
-        [])
-
-    const hasLoadedIcons = (
-        <Row>
-        {taskData.map((task) => {
-                <Col sm={4}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Body key={task.id}>
-                            <Card.Title>{task.title}</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>})}
-        </Row>
-    )  
-    const notLoadedIcons = (
-        <Avatar src={loader} height={55} />
-    )
-
+const TaskCard = (props) => {
+    const {title, important, owner_name, due_date, progress} = props
   return (
-    <div>
-        {hasLoaded ? hasLoadedIcons : notLoadedIcons}
-    </div>
+    <Card>
+        <div>
+            <h3 className='card-title'>{title}</h3>
+            <p>{owner_name}</p>
+        </div>
+        <div>
+            <p className='card-text'>{important}</p>
+            <p className='card-text'>{due_date}</p>
+            <p className='card-text'>{progress}</p>
+        </div>
+    </Card>
   )
 }
 
