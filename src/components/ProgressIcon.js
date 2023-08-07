fdf04c23c68f1036903900917e7bc6246a0d6da6
alcl2000 <amy.555.lewis@gmail.com>
2023-08-07 10:14:51 +0000

    add: progress icon component

---
+++ src/components/ProgressIcon.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProgressIcon = ({status, height}) => {
+
+    const backgroundColor = (status) => {
+        let backgroundColor
+        if (status === 'not_started'){
+            backgroundColor = 'bg-danger';
+        } else if ( status === 'in_progress'){
+            backgroundColor = 'bg-warning'
+        } else{
+            backgroundColor = 'bg-success'
+        }
+        return backgroundColor
+    }
+  return (
+    <div style={{height:{height}, width:{height}}} className={`rounded-circle ${backgroundColor(status)}`}></div>
+  )
+}
+
+export default ProgressIcon
