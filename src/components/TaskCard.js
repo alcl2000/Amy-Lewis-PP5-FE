bb1bd1a1e92ec565d90f173d9c5f72a374b986de
alcl2000 <amy.555.lewis@gmail.com>
2023-08-07 17:09:42 +0000

    style: taskcard styling

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
 import ProgressIcon from './ProgressIcon'
+import star from '../assets/star.svg'
+import styles from '../styles/TaskCard.module.css'
 
 const TaskCard = (props) => {
     const {title, important, owner_name, due_date, progress} = props
   return (
-    <Card>
-        <div className='row'>
-            <div className='col-7'>
-                <h4>{title}</h4>
+    <Card className={styles.Body}>
+        <div className={styles.Content}>
+            {important ? <img src={star} height={30}/>: <></>}
+            <div className='row'>
+                <div className='col-7'>
+                    <h4>{title}</h4>
+                </div>
+                <div className='col-5'>
+                    <p>Assigned to: {owner_name}</p>
+                </div>
             </div>
-            <div className='col-5'>
-                <p>Assigned to: {owner_name}</p>
+            <div>
+                <p className='card-text'>Due date: {due_date}</p>
+                <ProgressIcon status={progress} height={20} />
             </div>
-        </div>
-        <div>
-            <p className='card-text'>{important}</p>
-            <p className='card-text'>{due_date}</p>
-            <ProgressIcon status={progress} height={20} />
         </div>
     </Card>
   )
